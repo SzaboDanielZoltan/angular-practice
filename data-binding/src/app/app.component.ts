b2f744d5c7d930b85e2c4fd6f49036e242ef3a4d
Szabo Daniel Zoltan <szabo.daniel.zoltan@gmail.com>
2019-07-31 14:28:34 +0200

    Solved

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = `angular-practice-001`;
 
+  getName() {
+    return `Bond, James Bond`;
+  }
 }
